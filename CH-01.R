3cc9463db671c76afc6a3a9156eaf9f693b91731
Ali <razaalirafique@gmail.com>
2023-12-22 20:56:49 -0500

    update

--- CH-01.R
+++ CH-01.R
@@ -1,7 +1,14 @@
+# CH-01----
+
 library(mlr3)
-task = tsk("penguins")
-split = partition(task)
-learner = lrn("classif.rpart")
+task <- tsk("penguins")
+split <- partition(task)
+learner <- lrn("classif.rpart")
 
 learner$train(task, row_ids = split$train)
 learner$model
+
+prediction <- learner$predict(task, row_ids = split$test)
+prediction
+
+prediction$score(msr("classif.acc"))
